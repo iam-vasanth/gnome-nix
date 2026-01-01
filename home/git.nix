d7e1b7f296f16c3e9ff12c9518e52c984f0e1010
iam-vasanth <vk.vasanth@gmail.com>
2026-01-01 07:25:46 +0530

    Added signed commits for github

--- home/git.nix
+++ home/git.nix
@@ -3,6 +3,11 @@
 {
   programs.git = {
     enable = true;
+    signing = {
+      key = "${config.home.homeDirectory}/.ssh/github_ssh.pub";
+      signByDefault = true;
+      format = "ssh";
+    };
     ignores = [
       "*.swp"
       "*~"
@@ -28,28 +33,23 @@
       };
 
       url = {
-        "git@github.com:" = {
-          insteadOf = "https://github.com/";
-        };
-        "git@gitlab.com:" = {
-          insteadOf = "https://gitlab.com/";
-        };
+        "git@github.com:" = { insteadOf = "https://github.com/"; };
+        "git@gitlab.com:" = { insteadOf = "https://gitlab.com/"; };
       };
 
       extraConfig = {
-        init = {
-          defaultBranch = "main";
-        };
-        core = {
-          editor = "zed --wait";
-        };
-        pull = {
-          rebase = true;
-        };
-        push = {
-          autoSetupRemote = true;
-        };
+        init.defaultBranch = "main";
+        core.editor = "zed --wait";
+        pull.rebase = true;
+        push.autoSetupRemote = true;
+        "gpg.ssh".allowedSignersFile = "${config.home.homeDirectory}/.config/git/allowed_signers";
+        tag.gpgSign = true;
       };
     };
   };
+  home.file.".config/git/allowed_signers".text = let
+    pubKey = builtins.readFile "${config.home.homeDirectory}/.ssh/github_ssh.pub";
+  in ''
+    vk.vasanth.r@gmail.com namespaces="git" ${pubKey}
+  '';
 }
